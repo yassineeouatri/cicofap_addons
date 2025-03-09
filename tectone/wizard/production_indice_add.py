2de14f2247f2890e7a6955bd48218ba6981101d7
Yassine Elouatri <yassineelouatri@gmail.com>
2025-03-09 10:20:25 +0000

    Add possibility to add indice date from wizard

--- tectone/wizard/production_indice_add.py
+++ tectone/wizard/production_indice_add.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, models, fields
 from datetime import datetime
+from odoo import api, models, fields
+from odoo.exceptions import AccessError, ValidationError, UserError
 
+TODAY_DATE = datetime.today().strftime('%Y-%m-%d')
 
 class production_document_indice_add(models.Model):
     _name = 'production.document.indice.add'
 
     document_id = fields.Many2one('production.document', 'Document', required=True, ondelete='cascade')
+    date_indice = fields.Date('Date Indice', default=TODAY_DATE, required=True)
     no_indice = fields.Integer('Nb indices', default=1, required=True)
 
     def action_done(self):
+        if self.date_indice.strftime('%Y-%m-%d') > TODAY_DATE:
+            raise UserError("Vous ne pouvez pas créer un indice avec une date supérieure à la date courante!")
+
         indice = -1
         document_indice_obj = self.env['production.document.indice'].search([('document_id', '=', self.document_id.id)],
                                                                             order='indice desc', limit=1)
@@ -19,7 +25,7 @@ class production_document_indice_add(models.Model):
 
         document_obj = self.env['production.document'].search([('id', '=', self.document_id.id)])
         if self.document_id.state == 'draft':
-            document_obj.write({'indice': "{:02d}".format(indice + 1)})
+            document_obj.write({'indice': "{:02d}".format(indice + 1), 'date_indice': self.date_indice})
 
             document_indice_ids = self.env['production.document.indice'].search(
                 [('document_id', '=', self.document_id.id)])
@@ -27,7 +33,7 @@ class production_document_indice_add(models.Model):
                 record.write({'actif': False})
 
             self.env['production.document.indice'].create({'document_id': self.document_id.id,
-                                                           'date': datetime.today(),
+                                                           'date': self.date_indice,
                                                            'nature': '*',
                                                            'actif': True,
                                                            'indice': "{:02d}".format(indice + 1)})
